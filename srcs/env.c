629c80c7db36e61c9d87ccfa58ef361219f21a7a
Naphiye <Naphiye@gmail.com>
2024-06-16 12:26:58 +0200

    + env.c

---
+++ srcs/env.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsalomon <fsalomon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/16 12:09:52 by fsalomon          #+#    #+#             */
+/*   Updated: 2024/06/16 12:26:54 by fsalomon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/minishell.h"
+
+static void	ft_putendl_fd(char *s, int fd)
+{
+	int	i;
+
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i] != 0)
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+	write(fd, "\n", 1);
+}
+
+void	env(char *envp[], int fd)
+{
+	int i;
+
+	i = 0;
+	while (*envp != NULL)
+	{
+		ft_putendl_fd(*envp, fd);
+		envp ++;
+	}
+	return ;
+}
+
+
+// int main (int argc, char **argv, char *envp[])
+// {
+// 	(void)argc;
+// 	(void)argv;
+// 	env(envp, STDOUT_FILENO);
+// 	return (1);
+// }
