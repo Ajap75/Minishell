967de649648711799c122d91f145a163d3dfd049
Antoine Astruc <anastruc@paul-f5Ar2s1.clusters.42paris.fr>
2024-06-26 14:32:56 +0200

    lst_env

--- srcs/lst_env.c
+++ srcs/lst_env.c
@@ -6,69 +6,36 @@
 /*   By: anastruc <anastruc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 14:19:29 by anastruc          #+#    #+#             */
-/*   Updated: 2024/06/21 17:57:34 by anastruc         ###   ########.fr       */
+/*   Updated: 2024/06/26 14:22:16 by anastruc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
 
-typedef struct s_env
+void	print_env_node(t_env *node)
 {
-	char			*var_name;
-	char			*var_value;
-	struct s_env	*prev;
-	struct s_env	*next;
-}					t_env;
-
-t_env	*ft_lstnew_node_env(void *var_name, void *var_value)
-{
-	t_env	*new;
-
-	new = malloc(sizeof(*new));
-	if (!new)
-		return (NULL);
-	new->var_name = var_name;
-	new->var_value = var_value;
-	new->next = NULL;
-	return (new);
+	printf("Var_name : %s\n", node->var_name);
+	printf("Var_value : %s\n\n", node->var_value);
 }
-t_env	**ft_create_lst_env(char *envp[])
-{
-	t_env	**lst_env;
-	t_env	*new;
-	char	**env;
-	char	*var_name;
-	char	*var_value;
-	int		len;
-	int		i;
 
-	env = NULL;
-	if (envp == NULL)
-		env = ft_get_partial_env();
-	else
-		env = ft_get_full_env(envp);
-	while (*env)
+void	print_env_lst(t_env *node)
+{
+	t_env	*tmp;
+
+	tmp = node;
+	// printf("node\n");
+	// fflush(NULL);
+	while (tmp)
 	{
-		i = 0;
-		len = ft_strlen(*env);
-		while (*env[i] != '=' && *env[i])
-			i++;
-		var_name = ft_substr(*env, 0, i);
-		var_value = ft_substr(*env, (len - i), len);
-		new = ft_lstnew_node_env(var_name, var_value);
-		ft_lstadd_back(lst_env, new);
-		free(var_name);
-		free(var_value);
-		env++;
+		print_env_node(tmp);
+		tmp = tmp->next;
 	}
-	return (lst_env);
 }
-
 char	**ft_get_partial_env(void)
 {
 	char	**partial_env;
 	int		etc_env_fd;
-	char 	*line;
+	char	*line;
 	int		i;
 
 	line = NULL;
@@ -87,29 +54,109 @@ char	**ft_get_partial_env(void)
 	return (partial_env);
 }
 
-char	**ft_get_full_env(char **envp)
-{
-	char	**full_env;
+// char	**ft_get_full_env(char **envp)
+// {
+// 	char	**full_env;
 
-	full_env = NULL;
-	full_env = ft_split(envp, "\n");
-	return (full_env);
+// 	full_env = NULL;
+// 	full_env = ft_split(envp, "\n");
+// 	return (full_env);
+// }
+
+t_env	*ft_lstnew_node_env(void *var_name, void *var_value)
+{
+	t_env	*new;
+
+	new = malloc(sizeof(*new));
+	if (!new)
+		return (NULL);
+	new->var_name = var_name;
+	new->var_value = var_value;
+	new->next = NULL;
+	return (new);
 }
 
-// int	main(int argc, char **argv)
-// {
-// 	int len;
-// 	int i;
+void	ft_envadd_back(t_env **env_lst, t_env *new_node)
+{
+	t_env	*current;
 
-// 	if (argc == 2)
-// 	{
-// 		len = ft_strlen(argv[1]);
-// 		i = 0;
+	// print_env_node(new_node);
+	if (env_lst)
+	{
+		if (*env_lst)
+		{
+			current = *env_lst;
+			while (current->next)
+			{
+				current = current->next;
+			}
+			current->next = new_node;
+		}
+		else
+		{
+			if (new_node == NULL)
+				*env_lst = NULL;
+			else
+				*env_lst = new_node;
+		}
+	}
+}
+t_env	*ft_create_lst_env(char *envp[])
+{	// lst_env++;
 
-// 		while (argv[1][i] != '=' && argv[1][i])
-// 			i++;
-// 		printf("var name = %s\n", ft_substr(argv[1], 0, (i)));
-// 		printf("var value = %s\n", ft_substr(argv[1], (len - i), len));
-// 	}
-// 	return (0);
-// }
+	t_env	*lst_env;
+	t_env	*new;
+	char	**env;
+	char	*var_name;
+	char	*var_value;
+	int		len;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	env = NULL;
+	lst_env = NULL;
+	if (envp == NULL)
+		env = ft_get_partial_env();
+	env = envp;
+	while (env[i])
+	{
+		len = ft_strlen(env[i]);
+		while (env[i][j] != '\0' && env[i][j] != '=')
+			{
+				// printf("%c\n", env[i][j]);
+				// fflush(NULL);
+				j++;
+			}
+		var_name = ft_substr(env[i], 0, j);
+		var_value = ft_substr(env[i], (j + 1), (len - j));
+		new = ft_lstnew_node_env(var_name, var_value);
+		ft_envadd_back(&lst_env, new);
+		// free(var_name);
+		// free(var_value);
+		j = 0;
+		i++;
+	}
+	// lst_env++;
+	print_env_lst(lst_env);
+	printf("prout\n");
+	fflush(NULL);
+	return (lst_env);
+
+}
+
+
+
+int	main(int argc, char **argv, char *envp[])
+{
+	t_env	*lst_env;
+	(void)argv;
+
+	if (argc >= 1)
+	{
+		lst_env = ft_create_lst_env(envp);
+		// print_env_lst(lst_env);
+	}
+	return (0);
+}
