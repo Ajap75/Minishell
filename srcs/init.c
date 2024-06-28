0588b16f5e21131cc051aaf669651425ec00b8b9
Antoine Astruc <anastruc@paul-f5Ar2s3.clusters.42paris.fr>
2024-06-28 10:44:25 +0200

    prompt

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: anastruc <anastruc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 12:22:39 by fsalomon          #+#    #+#             */
-/*   Updated: 2024/06/27 17:15:11 by anastruc         ###   ########.fr       */
+/*   Updated: 2024/06/28 10:02:21 by anastruc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,12 @@
 
 t_data	*ft_init_data(char *envp[])
 {
-	static t_data *minishell;
+	static t_data	*minishell;
+
 	minishell = malloc(sizeof(t_data));
 	minishell = get_data();
 	ft_bzero(minishell, sizeof(t_data));
 	minishell->env = get_env(envp);
 	print_env_lst(minishell->env);
-
-	printf("prout\n");
 	return (minishell);
 }
-
-
--- srcs/main.c
+++ srcs/main.c
@@ -6,22 +6,41 @@
 /*   By: anastruc <anastruc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 11:19:18 by fsalomon          #+#    #+#             */
-/*   Updated: 2024/06/27 17:13:46 by anastruc         ###   ########.fr       */
+/*   Updated: 2024/06/28 10:44:09 by anastruc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "../headers/minishell.h"
 
 int	main(int argc, char **argv, char *envp[])
 {
-	static t_data *minishell;
-	minishell = malloc(sizeof(t_data));
+	char	*input;
 
+	// r1_bind_key('\t', r1_complete); //
+	while (1)
+	{
+		input = readline("Minishell > ");
+		if (input == NULL)
+		{
+			printf("Doit quitter le prog ex ctrl_D\n");
+			break ;
+		}
+		if (*input)
+		{
+			add_history(input);
+			return (run_minishell(input, envp));
+		}
+	}
+	return (1);
+}
+
+int	run_minishell(char *command_line, char *envp[])
+{
+	// t_data data_to_exec;
+	// data_to_exec = ft_init_data(envp);
+	// exec(data_to_exec);
+	t_data	*minishell;
+	minishell = malloc(sizeof(t_data));
 	minishell = ft_init_data(envp);
 	printf("ENVP : \n%s\n", minishell->env->var_name);
-	(void)argc;
-	(void)argv;
-	(void)envp;
-	return (1);
 }
